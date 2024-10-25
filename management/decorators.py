21fd4be645c79fb48c772b0b7437adc0322f961a
karim_laptop <damorekarim7@gmail.com>
2024-10-25 15:46:53 +0200

    Now the SellerRequiredMixin checks also if the current seller is the one that added the laptop

--- management/decorators.py
+++ management/decorators.py
@@ -1,6 +1,10 @@
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from functools import wraps
+
+from management.models import Laptop
+
 
 # Decorator per le FBV
 def seller_required(view_func):
@@ -12,8 +16,11 @@ def seller_required(view_func):
     return _wrapped_view
 
 # Mixin per le CBV
+#Controlla sia che lo user sia un seller, sia che il seller corrente sia colui che ha aggiunto il laptop
 class SellerRequiredMixin(LoginRequiredMixin):
     def dispatch(self, request, *args, **kwargs):
+        laptop = get_object_or_404(Laptop, pk=kwargs['pk'])
         if request.user.is_authenticated and request.user.user_type == 'seller':
-            return super().dispatch(request, *args, **kwargs)
-        raise PermissionDenied("Accesso riservato ai fornitori")
+            if laptop.seller_id == request.user.id:
+                return super().dispatch(request, *args, **kwargs)
+        raise PermissionDenied("Accesso riservato ai fornitori oppure fornitore corrente diverso dal fornitore che ha aggiunto questo laptop")
