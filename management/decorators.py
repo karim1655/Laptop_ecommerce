ad70175c4d8734441d8cd91d62585708ea2ed792
karim_laptop <damorekarim7@gmail.com>
2024-10-26 11:17:24 +0200

    Removed superuser permissions ("or request.user.is_superuser" in decorators and "or user.is_superuser" in the templates)

--- management/decorators.py
+++ management/decorators.py
@@ -10,7 +10,7 @@ from management.models import Laptop
 def seller_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.user_type == 'seller' or request.user.is_superuser:
+        if request.user.is_authenticated and request.user.user_type == 'seller':
             return view_func(request, *args, **kwargs)
         raise PermissionDenied("Accesso riservato ai fornitori")
     return _wrapped_view
@@ -20,7 +20,7 @@ def seller_required(view_func):
 class SellerRequiredMixin(LoginRequiredMixin):
     def dispatch(self, request, *args, **kwargs):
         laptop = get_object_or_404(Laptop, pk=kwargs['pk'])
-        if request.user.is_authenticated and request.user.user_type == 'seller' or request.user.is_superuser:
-            if laptop.seller_id == request.user.id or request.user.is_superuser:
+        if request.user.is_authenticated and request.user.user_type == 'seller':
+            if laptop.seller_id == request.user.id:
                 return super().dispatch(request, *args, **kwargs)
         raise PermissionDenied("Accesso riservato ai fornitori oppure fornitore corrente diverso dal fornitore che ha aggiunto questo laptop")
