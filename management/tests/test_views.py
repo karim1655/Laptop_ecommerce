d10644355470de334c2389c352d183c4b1bc9d28
karim_laptop <damorekarim7@gmail.com>
2024-10-31 18:29:45 +0100

    Added tests for cart and checkout features, everything works fine

--- management/tests/test_views.py
+++ management/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from management.models import Laptop, LaptopReview, SellerReview
+from management.models import Laptop, LaptopReview, SellerReview, Cart, CartItem, Order, OrderItem
 
 User = get_user_model()
 
@@ -316,3 +316,121 @@ class SellerReviews(TestCase):
         self.assertRedirects(response, reverse('home'))
         # Assicura che la recensione non sia stata creata
         self.assertFalse(SellerReview.objects.filter(seller=self.seller, user=self.seller).exists())
+
+
+
+class CartViewsTests(TestCase):
+    def setUp(self):
+        # Creazione di utenti buyer e seller
+        self.buyer = User.objects.create_user(username='buyer', password='password', user_type='buyer')
+        self.seller = User.objects.create_user(username='seller', password='password', user_type='seller')
+
+        # Creazione di un laptop
+        self.laptop = Laptop.objects.create(
+            name='Test Laptop',
+            processor_brand='Intel',
+            processor_model='i5',
+            ram=8,
+            storage=256,
+            display_inches=15,
+            price=1000,
+            category='GA',
+            seller=self.seller
+        )
+
+        # Creazione di un carrello per il buyer
+        self.cart = Cart.objects.create(user=self.buyer)
+
+    def test_add_to_cart_as_buyer(self):
+        self.client.login(username='buyer', password='password')
+        response = self.client.post(reverse('add_to_cart', args=[self.laptop.id]))
+
+        # Verifica del reindirizzamento e della presenza di CartItem
+        self.assertEqual(response.status_code, 302)
+        cart_item = CartItem.objects.filter(cart=self.cart, laptop=self.laptop).first()
+        self.assertIsNotNone(cart_item)
+        self.assertEqual(cart_item.quantity, 1)
+
+    def test_add_to_cart_as_seller_redirects(self):
+        self.client.login(username='seller', password='password')
+        response = self.client.post(reverse('add_to_cart', args=[self.laptop.id]))
+
+        # Verifica che il seller venga reindirizzato alla home e che non sia stato creato nessun CartItem
+        self.assertRedirects(response, reverse('home'))
+        self.assertFalse(CartItem.objects.filter(cart__user=self.seller).exists())
+
+    def test_cart_detail_as_buyer(self):
+        self.client.login(username='buyer', password='password')
+        response = self.client.get(reverse('cart_detail'))
+
+        # Verifica che la risposta contenga la pagina del carrello
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Carrello')
+
+    def test_cart_detail_as_seller_redirects(self):
+        self.client.login(username='seller', password='password')
+        response = self.client.get(reverse('cart_detail'))
+
+        # Verifica che il seller venga reindirizzato alla home
+        self.assertRedirects(response, reverse('home'))
+
+    def test_increase_quantity(self):
+        # Aggiunta iniziale di un laptop al carrello
+        CartItem.objects.create(cart=self.cart, laptop=self.laptop, quantity=1)
+
+        self.client.login(username='buyer', password='password')
+        response = self.client.post(reverse('increase_quantity', args=[self.laptop.id]))
+
+        # Verifica che la quantità sia aumentata
+        cart_item = CartItem.objects.get(cart=self.cart, laptop=self.laptop)
+        self.assertEqual(cart_item.quantity, 2)
+
+    def test_decrease_quantity(self):
+        # Aggiunta iniziale di un laptop al carrello
+        CartItem.objects.create(cart=self.cart, laptop=self.laptop, quantity=2)
+
+        self.client.login(username='buyer', password='password')
+        response = self.client.post(reverse('decrease_quantity', args=[self.laptop.id]))
+
+        # Verifica che la quantità sia diminuita
+        cart_item = CartItem.objects.get(cart=self.cart, laptop=self.laptop)
+        self.assertEqual(cart_item.quantity, 1)
+
+        # Decrementa la quantità a zero e verifica l'eliminazione del CartItem
+        response = self.client.post(reverse('decrease_quantity', args=[self.laptop.id]))
+        self.assertFalse(CartItem.objects.filter(cart=self.cart, laptop=self.laptop).exists())
+
+    def test_remove_from_cart(self):
+        CartItem.objects.create(cart=self.cart, laptop=self.laptop, quantity=1)
+
+        self.client.login(username='buyer', password='password')
+        response = self.client.post(reverse('remove_from_cart', args=[self.laptop.id]))
+
+        # Verifica che il CartItem sia stato eliminato
+        self.assertFalse(CartItem.objects.filter(cart=self.cart, laptop=self.laptop).exists())
+
+    def test_checkout_view(self):
+        CartItem.objects.create(cart=self.cart, laptop=self.laptop, quantity=1)
+
+        self.client.login(username='buyer', password='password')
+        response = self.client.get(reverse('checkout'))
+
+        # Verifica che la pagina di checkout mostri gli articoli del carrello
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Riepilogo Ordine')
+
+    def test_confirm_order(self):
+        CartItem.objects.create(cart=self.cart, laptop=self.laptop, quantity=1)
+
+        self.client.login(username='buyer', password='password')
+        response = self.client.post(reverse('confirm_order'))
+
+        # Verifica che l'ordine sia stato creato e che il carrello sia svuotato
+        self.assertEqual(Order.objects.filter(user=self.buyer).count(), 1)
+        self.assertFalse(CartItem.objects.filter(cart=self.cart).exists())
+
+        # Verifica che OrderItem sia stato creato con il laptop e la quantità corretti
+        order = Order.objects.get(user=self.buyer)
+        order_item = OrderItem.objects.get(order=order, laptop=self.laptop)
+        self.assertEqual(order_item.quantity, 1)
+        self.assertEqual(order_item.price, self.laptop.price)
