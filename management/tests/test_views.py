da53b8d4d1964a94a86943cd7138154c5998ef23
karim_laptop <damorekarim7@gmail.com>
2024-10-31 01:27:05 +0100

    Restructured tests and added tests: add_laptop_as_seller_with_image, search_partial_match, search_case_insensitive

--- management/tests/test_views.py
+++ management/tests/test_views.py
@@ -6,8 +6,8 @@ from management.models import Laptop, LaptopReview, SellerReview
 User = get_user_model()
 
 
-# TEST PER LE VIEWS
-class UserTests(TestCase):
+
+class AuthTests(TestCase):
     def setUp(self):
         self.buyer = User.objects.create_user(username='buyer', password='password', user_type='buyer')
         self.seller = User.objects.create_user(username='seller', password='password', user_type='seller')
@@ -70,8 +70,25 @@ class UserTests(TestCase):
         # Verifica che il messaggio di errore corretto sia presente nella risposta
         self.assertContains(response, 'Please enter a correct username and password. Note that both fields may be case-sensitive.')
 
-    def test_add_laptop_as_seller(self):
-        self.client.login(username='seller', password='password')
+
+
+class LaptopCreateViewTests(TestCase):
+    def setUp(self):
+        self.buyer = User.objects.create_user(username='buyer', password='password1', user_type='buyer')
+        self.seller = User.objects.create_user(username='seller', password='password1', user_type='seller')
+        self.laptop = Laptop.objects.create(
+            name='Test Laptop',
+            processor_brand='Intel',
+            processor_model='i5',
+            ram=8,
+            storage=256,
+            display_inches=15,
+            price=800.00,
+            category='BL',
+            seller=self.seller
+        )
+    def test_add_laptop_as_seller_without_image(self):
+        self.client.login(username='seller', password='password1')
         response = self.client.post(reverse('add_laptop'), {
             'name': 'New Laptop',
             'processor_brand': 'AMD',
@@ -82,12 +99,29 @@ class UserTests(TestCase):
             'price': 1200.00,
             'category': 'GA',
         })
-        self.assertEqual(response.status_code, 302)  # Should redirect
+        self.assertEqual(response.status_code, 302)
         self.assertTrue(Laptop.objects.filter(name='New Laptop').exists())
+
+    def test_add_laptop_as_seller_with_image(self):
+        self.client.login(username='seller', password='password1')
+        with open(r'C:\Users\damor\PycharmProjects\Laptop_ecommerce\management\tests\files\mba13-m2-midnight-gallery1-202402.jpeg', 'rb') as image:
+            response = self.client.post(reverse('add_laptop'), {
+                'name': 'New Laptop with image',
+                'processor_brand': 'Intel',
+                'processor_model': 'i7',
+                'ram': 16,
+                'storage': 512,
+                'display_inches': 15,
+                'price': 1200.00,
+                'image': image,
+                'category': 'GA',
+            })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Laptop.objects.filter(name='New Laptop with image').exists())
 
     def test_add_laptop_as_buyer_forbidden(self):
         # Effettua il login come buyer
-        self.client.login(username='buyer', password='password')
+        self.client.login(username='buyer', password='password1')
         # Tenta di creare un laptop
         response = self.client.post(reverse('add_laptop'), {
             'name': 'Forbidden Laptop',
@@ -105,7 +139,7 @@ class UserTests(TestCase):
         self.assertFalse(Laptop.objects.filter(name='Forbidden Laptop').exists())
 
     def test_add_laptop_with_missing_fields(self):
-        self.client.login(username='seller', password='password')
+        self.client.login(username='seller', password='password1')
         response = self.client.post(reverse('add_laptop'), {
             'name': '',  # Nome vuoto
             'processor_brand': 'AMD',
@@ -127,8 +161,26 @@ class UserTests(TestCase):
         # Assicura che il messaggio di errore sia corretto
         self.assertIn('This field is required.', form.errors['name'])
 
+
+
+class ReviewsTests(TestCase):
+    def setUp(self):
+        self.buyer = User.objects.create_user(username='buyer', password='password2', user_type='buyer')
+        self.seller = User.objects.create_user(username='seller', password='password2', user_type='seller')
+        self.laptop = Laptop.objects.create(
+            name='Test Laptop',
+            processor_brand='Intel',
+            processor_model='i5',
+            ram=8,
+            storage=256,
+            display_inches=15,
+            price=800.00,
+            category='BL',
+            seller=self.seller
+        )
+
     def test_add_laptop_review(self):
-        self.client.login(username='buyer', password='password')
+        self.client.login(username='buyer', password='password2')
         response = self.client.post(reverse('laptop_review', args=[self.laptop.id]), {
             'rating': 5,
             'description': 'Great laptop!'
@@ -136,8 +188,26 @@ class UserTests(TestCase):
         self.assertEqual(response.status_code, 302)  # Dovrebbe ridirezionare
         self.assertTrue(LaptopReview.objects.filter(laptop=self.laptop, user=self.buyer).exists())
 
+
+
+class DashboardTests(TestCase):
+    def setUp(self):
+        self.buyer = User.objects.create_user(username='buyer', password='password3', user_type='buyer')
+        self.seller = User.objects.create_user(username='seller', password='password3', user_type='seller')
+        self.laptop = Laptop.objects.create(
+            name='Test Laptop',
+            processor_brand='Intel',
+            processor_model='i5',
+            ram=8,
+            storage=256,
+            display_inches=15,
+            price=800.00,
+            category='BL',
+            seller=self.seller
+        )
+
     def test_access_seller_dashboard(self):
-        self.client.login(username='seller', password='password')
+        self.client.login(username='seller', password='password3')
         response = self.client.get(reverse('seller_dashboard', args=[self.seller.id]))
         self.assertEqual(response.status_code, 200)  # Dovrebbe ritornare la pagina dashboard
 
@@ -146,9 +216,26 @@ class UserTests(TestCase):
         self.assertEqual(response.status_code, 302)  # Dovrebbe ridirezionare al login
 
     def test_access_seller_dashboard_as_buyer_forbidden(self):
-        self.client.login(username='buyer', password='password')
+        self.client.login(username='buyer', password='password3')
         response = self.client.get(reverse('seller_dashboard', args=[self.seller.id]))
         self.assertEqual(response.status_code, 403)  # Dovrebbe riornare forbidden
+
+
+class LaptopDetailViewTests(TestCase):
+    def setUp(self):
+        self.buyer = User.objects.create_user(username='buyer', password='password4', user_type='buyer')
+        self.seller = User.objects.create_user(username='seller', password='password4', user_type='seller')
+        self.laptop = Laptop.objects.create(
+            name='Test Laptop',
+            processor_brand='Intel',
+            processor_model='i5',
+            ram=8,
+            storage=256,
+            display_inches=15,
+            price=800.00,
+            category='BL',
+            seller=self.seller
+        )
 
     def test_laptop_detail_view(self):
         response = self.client.get(reverse('laptop_detail', args=[self.laptop.id]))
@@ -158,13 +245,58 @@ class UserTests(TestCase):
         response = self.client.get(reverse('laptop_detail', args=[self.laptop.id]))
         self.assertEqual(response.status_code, 200)  # Dovrebbe ritornare la pagina laptop detail
 
-    def test_search_laptops(self):
+
+class SearchTests(TestCase):
+    def setUp(self):
+        self.buyer = User.objects.create_user(username='buyer', password='password5', user_type='buyer')
+        self.seller = User.objects.create_user(username='seller', password='password5', user_type='seller')
+        self.laptop = Laptop.objects.create(
+            name='Test Laptop',
+            processor_brand='Intel',
+            processor_model='i5',
+            ram=8,
+            storage=256,
+            display_inches=15,
+            price=800.00,
+            category='BL',
+            seller=self.seller
+        )
+
+    def test_search_exact_match(self):
         response = self.client.get(reverse('search'), {'name': 'Test Laptop'})
-        self.assertEqual(response.status_code, 200)  # Dovrebbe ritornare search results
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Test Laptop')
 
+    def test_search_partial_match(self):
+        response = self.client.get(reverse('search'), {'name': 'Laptop'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Laptop')
+
+    def test_search_case_insensitive(self):
+        response = self.client.get(reverse('search'), {'name': 'test laptop'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Laptop')
+
+
+
+class SellerReviews(TestCase):
+    def setUp(self):
+        self.buyer = User.objects.create_user(username='buyer', password='password6', user_type='buyer')
+        self.seller = User.objects.create_user(username='seller', password='password6', user_type='seller')
+        self.laptop = Laptop.objects.create(
+            name='Test Laptop',
+            processor_brand='Intel',
+            processor_model='i5',
+            ram=8,
+            storage=256,
+            display_inches=15,
+            price=800.00,
+            category='BL',
+            seller=self.seller
+        )
+
     def test_add_seller_review(self):
-        self.client.login(username='buyer', password='password')
+        self.client.login(username='buyer', password='password6')
         response = self.client.post(reverse('seller_review', args=[self.seller.id, self.laptop.id]), {
             'rating': 4,
             'description': 'Good seller!'
@@ -174,7 +306,7 @@ class UserTests(TestCase):
 
     def test_add_seller_review_as_seller_redirect(self):
         # Effettua il login come seller
-        self.client.login(username='seller', password='password')
+        self.client.login(username='seller', password='password6')
         # Tenta di aggiungere una recensione per un altro seller
         response = self.client.post(reverse('seller_review', args=[self.seller.id, self.laptop.id]), {
             'rating': 3,
